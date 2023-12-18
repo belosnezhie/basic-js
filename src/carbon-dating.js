f583a8d63c6b1ed8e6d338f169dc95a0d97ce700
Maria Treier <mariatraier@gmail.com>
2023-12-18 19:42:20 +0100

    feat: complete part of the task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,20 +6,30 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string'
+      || Number(sampleActivity) <= 0
+      ||  Number(sampleActivity) > MODERN_ACTIVITY
+      ||  isNaN(sampleActivity) === true) {
+    return false;
+  }
+
+  let result;
+
+  result = (Math.log(MODERN_ACTIVITY / Number(sampleActivity)) * HALF_LIFE_PERIOD) / Math.log(2);
+
+  return Math.ceil(result);
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -2,20 +2,42 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (Array.isArray(members) === false) {
+    return false;
+  }
+
+  if (members.length === 0) {
+    return false;
+  }
+
+  const firstLetters = members.map((currentValue) => {
+    if (typeof currentValue === "string") {
+      const trimmedCurrentValue = currentValue.trim().toUpperCase();
+      return trimmedCurrentValue[0];
+    } else {
+      return false;
+    }
+  })
+
+  const filteredFirstLetters = firstLetters.filter((item) => item !== false);
+
+  filteredFirstLetters.sort();
+
+  const result = filteredFirstLetters.join("");
+
+  return result;
 }
 
 module.exports = {
